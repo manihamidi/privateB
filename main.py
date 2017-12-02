8cb245a6f5097a4fb8d1091b444d12fd1c5a6e61
manihamidi <mani.hamidi@abcellera.com>
2017-12-01 17:21:19 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+def hello_from_b():
+    print("We are in the privary library B: privatelibb.\n"
+          "Library A is dependent on this library. It calls this very function actually!\n"
+          "If you see this message then the dependency has been satisfied.")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='privatelibb',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Mani',
+    author_email='',
+    description=''
+)
